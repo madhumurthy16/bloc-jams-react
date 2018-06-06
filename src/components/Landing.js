1514b207a7fec8711476dc10d124c40082b969c0
Madhu Murthy <msmurthy@msn.com>
2018-06-06 11:54:54 -0700

    Checkpoint 3 routing: completed

---
+++ src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// This is a functional component
+
+const Landing = () => (
+	<section className="landing">
+		Landing page will go here.
+	</section>
+
+);
+
+export default Landing;
---
+++ src/components/Library.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// This is a functional component
+
+const Library = () => (
+	<section>
+		Library page will go here.
+	</section>
+);
+
+export default Library;
