da7a7372ac9a49208fe488f7785b7133e1c19475
Madhu Murthy <msmurthy@msn.com>
2018-06-07 14:53:31 -0700

    Assignment 3 routing added Album component

---
+++ src/components/Album.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// This is a functional component
+
+const Album = () => (
+	<section className="landing">
+		Album page will go here.
+	</section>
+
+);
+
+export default Album;
