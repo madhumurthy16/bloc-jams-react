69d9ddcd95f1e4d512a6d8446ba57055d2521130
Madhu Murthy <msmurthy@msn.com>
2018-06-08 16:02:45 -0700

    Checkpoint 5 components:library completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-// This is a functional component
-
-const Album = () => (
-	<section className="landing">
-		Album page will go here.
-	</section>
-
-);
+class Album extends Component {
+	render() {
+		return (
+			<section className="album">
+				{this.props.match.params.slug} Album will go here
+			</section>
+		);
+	}
+}
 
 export default Album;
--- src/components/Library.js
+++ src/components/Library.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react'; 
+import { Link } from 'react-router-dom';
+import albumData from './../data/albums'; 
 
-// This is a functional component
+class Library extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { albums: albumData };
+	}
 
-const Library = () => (
-	<section>
-		Library page will go here.
-	</section>
-);
+	render() {
+		return (
+			<section className='library'>
+				{
+					this.state.albums.map((album,index) => 
+						//use a template literal to build a unique link path for each album based on a base URL, /album/ and the album's slug property
+						<Link to={`/album/${album.slug}`} key={index}> 
+							<img src={album.albumCover} alt={album.title} />
+							<div>{album.title}</div>
+							<div>{album.artist}</div>
+							<div>{album.songs.length} songs</div>
+						</Link>
+					)
+				}
+			</section>
+		);
+	}
+}
 
 export default Library;
