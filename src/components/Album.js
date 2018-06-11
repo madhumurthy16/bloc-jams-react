9813afa91076dcbf663d57e1abe18b831bd06ef0
Madhu Murthy <msmurthy@msn.com>
2018-06-11 15:05:16 -0700

    checkpoint 7 audio playback completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -15,43 +15,83 @@ class Album extends Component {
 
 		//set the state objects album property to the album retrived from above
 		this.state = {
-			album: album
+			album: album,
+			currentSong: album.songs[0],
+			isPlaying: false
 		};
+
+		//Create an audio element
+		this.audioElement = document.createElement('audio');
+		//Assign the first song's src to the audio element
+		this.audioElement.src = album.songs[0].audioSrc;
+
 	}
-		
-		render() {
-			return (
-				<section className="album">
-					<section id="album-info">
-						<img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.albumTitle} />
-						<div className="album-details">
-							<h1 id="album-title">{this.state.album.title}</h1>
-							<h2 className="artist">{this.state.album.artist}</h2>
-							<div id="release-info">{this.state.album.releaseInfo}</div>
-						</div>
-					</section>
-					<table id="song-list">
-						<colgroup>
-							<col id="song-number-column" />
-							<col id="song-title-column" />
-							<col id="song-duration-column" />
-						</colgroup>
-						<tbody>
-							{
-								this.state.album.songs.map(( song, index ) => 
-									<tr key={index}>
-										<td>{index+1}</td>
-										<td>{song.title}</td>
-										<td>{song.duration} secs</td>
-									</tr>
-								)
-							}
-						</tbody>
-					</table>
-				</section>
-			);
+
+	play() {
+		this.audioElement.play();
+		this.setState({isPlaying: true});
+	}
+
+	pause() {
+		this.audioElement.pause();
+		this.setState({isPlaying: false});
+	}
+
+	// Receives current song as a 'song' object and sets the audioElements src and the currentSong(in state) 
+	setSong(song) {
+		this.audioElement.src = song.audioSrc;
+		this.setState({ currentSong: song });
+	}
+
+	// Click event handler => determines to call play(), pause() or setState() 
+
+	handleSongClick(song) {
+		const isSameSong = this.state.currentSong === song;
+		if(this.state.isPlaying && isSameSong) {
+			this.pause();
+
+		}
+		else {
+			if ( !isSameSong ){
+				this.setSong(song);
+			}
+			this.play();
 		}
 	}
+
+		
+	render() {
+		return (
+			<section className="album">
+				<section id="album-info">
+					<img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.albumTitle} />
+					<div className="album-details">
+						<h1 id="album-title">{this.state.album.title}</h1>
+						<h2 className="artist">{this.state.album.artist}</h2>
+						<div id="release-info">{this.state.album.releaseInfo}</div>
+					</div>
+				</section>
+				<table id="song-list">
+					<colgroup>
+						<col id="song-number-column" />
+						<col id="song-title-column" />
+						<col id="song-duration-column" />
+					</colgroup>
+					<tbody>
+						{
+							this.state.album.songs.map(( song, index ) => 
+								<tr className="song" key={index} onClick={() => this.handleSongClick(song) }>										<td>{index+1}</td>
+									<td>{song.title}</td>
+									<td>{song.duration} secs</td>
+								</tr>
+							)
+						}
+					</tbody>
+				</table>
+			</section>
+		);
+	}
+}
 
 
 export default Album;
