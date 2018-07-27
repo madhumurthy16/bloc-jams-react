e4aee9ce4529bb2405968053e8484fde8ce22bcc
Madhu Murthy <msmurthy@msn.com>
2018-07-26 18:03:43 -0700

    Added Landing page styles

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,24 +4,40 @@ import React from 'react';
 
 const Landing = () => (
 	<section className="landing">
-		<h1>Turn the music up!</h1>
-		
-		<section className="selling-points">
-			<div className="point">
-				<h2 className="point-title">Choose your music</h2>
-				<p className="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
-			</div>
-			<div className="point">
-        		<h2 className="point-title">Unlimited, streaming, ad-free</h2>
-        		<p className="point-description">No arbitrary limits. No distractions.</p>
-      		</div>
-      		<div className="point">
-       			 <h2 className="point-title">Mobile enabled</h2>
-        		<p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
-      		</div>
 
+		<div className="card bg-dark text-white mb-5 border-0 rounded-0">
+			<img className="card-img rounded-0" src='/assets/images/ocean1.jpeg' alt="Card image"/>
+			<div className="card-img-overlay m-5">
+				<h1 className="card-title display-4">Turn the music up!</h1>
+				<p className="card-text lead">Anywhere Anytime</p>
+			</div>
+		</div>
+
+		<section className="selling-points container mb-5">
+			<div className="card-deck">
+				<div className="card">
+					<div className="card-body">
+						<h3 className="point-title card-title mb-4">Choose your music</h3>
+						<p className="point-description card-text">The world is full of music; why should you have to listen to music that someone else chose?</p>
+					</div>
+				</div>
+				<div className="card">
+					<div className="card-body">
+        				<h3 className="point-title card-title mb-4">Unlimited, streaming, ad-free</h3>
+        				<p className="point-description card-text">No arbitrary limits. No distractions.</p>
+        			</div>
+        		</div>
+      			<div className="card">
+					<div className="card-body">
+       			 		<h3 className="point-title card-title mb-4">Mobile enabled</h3>
+        				<p className="point-description card-text">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
+        			</div>
+        		</div>
+	      	</div>	
 		</section>
 	</section>
 );
 
 export default Landing;
+
+
