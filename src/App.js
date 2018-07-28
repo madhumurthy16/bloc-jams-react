948bc550bf57dd09a5776b8385c1018e67c7cf6d
Madhu Murthy <msmurthy@msn.com>
2018-07-27 23:49:33 -0700

    styling components

--- src/App.js
+++ src/App.js
@@ -10,21 +10,33 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header>
-          <nav>
-            <Link to='/'>Landing</Link>
-            <Link to='/library'>Library</Link>
+
+          <nav className="navbar navbar-expand-md navbar-light fixed-top bg-light">
+            <a className="navbar-brand" href="#">Swell Sounds</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav ml-auto">
+                  <Link className="nav-item nav-link active" to='/'>Landing<span class="sr-only">(current)</span></Link>
+                  <Link className="nav-item nav-link" to='/library'>Library</Link>
+              </div>
+            </div>
           </nav>
-          <h1>Bloc Jams</h1>
-        </header>
-        <main>
-          <Route exact path="/" component={Landing} />
-          <Route path="/library" component={Library} />
-          
-          {/*Route passes the slug to the Album component. 
-          It is then accessible through props as this.props.match.params.slug in the Album component*/}
-          <Route path="/album/:slug" component={Album} /> 
-        </main>
+
+          <main className="pt-5">
+            <Route exact path="/" component={Landing} />
+            <Route path="/library" component={Library} />
+            
+            {/*Route passes the slug to the Album component. 
+            It is then accessible through props as this.props.match.params.slug in the Album component*/}
+            <Route path="/album/:slug" component={Album} /> 
+          </main>
+          <hr/>
+          <footer className="container">
+            <p>© Swell Sounds 2018-2019</p>
+          </footer>
+
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
